bb715439697fa9396aefe5e1cfda4dc61018b238
paulfleurdelys <paul.fleurdelys@gmail.com>
2020-10-13 20:50:54 -0700

    Add data array, v-for, and data bindings in accordion

--- lesson07/assignment/restaurant/src/components/Accordion.vue
+++ lesson07/assignment/restaurant/src/components/Accordion.vue
@@ -1,53 +1,32 @@
 <template>
-         <!-- Accordion with text changed to dark-->
-
-            <div class="accordion" id="accordionExample">
-                <div class="card">
-                  <div class="card-header" id="headingOne">
-                    <h2 class="mb-0">
-                      <button class="text-dark btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        What Are Your Hours?
-                      </button>
-                    </h2>
-                  </div>
-              
-                  <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                    <div class="card-body">
-We're open daily from 11am to 9pm                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header" id="headingTwo">
-                    <h2 class="mb-0">
-                      <button class="text-dark btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Do You Take Reservations?
-                      </button>
-                    </h2>
-                  </div>
-                  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                    <div class="card-body">
-Yes, please reserve online. For groups larger than 6, please call us directly for special accomodations.                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header" id="headingThree">
-                    <h2 class="mb-0">
-                      <button class="text-dark btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Can I Bring My Own Alcohol?
-                      </button>
-                    </h2>
-                  </div>
-                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                    <div class="card-body">
-We offer corkage for $40 if you wish to bring your own wine.                    </div>
-                  </div>
-                </div>
-              </div>
+  <!-- Accordion with text changed to dark-->
+  <div class="accordion" id="accordionExample">
+    <AccordionItem v-for="item in items" 
+    v-bind:key="item.id"
+    v-bind:question="item.question"
+    v-bind:answer="item.answer"
+    v-bind:collapseTarget="item.collapseTarget" 
+    v-bind:dataTarget="item.dataTarget" />
+  </div>
 </template>
 
 <script>
+import AccordionItem from '@/components/AccordionItem.vue'
+
 export default {
   name: 'Accordion',
-  props: {}
+  components: {
+    AccordionItem
+  },
+  props: {},
+  data() {
+    return { items:
+      [
+        { id: 1, question: "What Are Your Hours?", answer: "We're open daily from 11am to 9pm", collapseTarget: "collapseOne", dataTarget: "#collapseOne" },
+        { id: 2, question: "Do You Take Reservations?", answer: "Yes, please reserve online. For groups larger than 6, please call us directly for special accomodations.", collapseTarget: "collapseTwo", dataTarget: "#collapseTwo"  },
+        { id: 3, question: "Can I Bring My Own Alcohol?", answer: "We offer corkage for $40 if you wish to bring your own wine.", collapseTarget: "collapseThree", dataTarget: "#collapseThree" }
+      ]
+    }
+  }
 }
 </script>
---
+++ lesson07/assignment/restaurant/src/components/AccordionItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h2 class="mb-0">
+        <button class="text-dark btn btn-link btn-block text-left" type="button" data-toggle="collapse" v-bind:data-target="dataTarget">
+          {{ question }}
+        </button>
+      </h2>
+    </div>
+
+    <div v-bind:id="collapseTarget" class="collapse">
+      <div class="card-body">
+        {{ answer }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccordionItem',
+  props: { 
+    question: String, 
+    answer: String, 
+    collapseTarget: String,
+    dataTarget: String 
+    }
+}
+</script>
