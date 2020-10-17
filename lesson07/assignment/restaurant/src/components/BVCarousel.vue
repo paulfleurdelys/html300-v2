7b3e5a6292a86b73eb67925485b2d09423d6886f
paulfleurdelys <paul.fleurdelys@gmail.com>
2020-10-17 13:40:53 -0700

    Updated carousel paths

--- lesson07/assignment/restaurant/src/components/BVCarousel.vue
+++ lesson07/assignment/restaurant/src/components/BVCarousel.vue
@@ -18,38 +18,17 @@
       <b-carousel-slide
         caption="First slide"
         text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="../assets/bar-984527_960_720.jpg'"
+        img-src="../assets/bar-984527_960_720.jpg"
       ></b-carousel-slide>
 
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="require('../assets/' + items[0].src)">
+        <!-- Slides with custom text -->
+      <b-carousel-slide img-src="../assets/wine-bottles-3623697_960_720.jpg">
         <h1>Hello world!</h1>
       </b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+      <b-carousel-slide img-src="../assets/coffee-918926_960_720.jpg"></b-carousel-slide>     
 
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
     </b-carousel>
 
     <p class="mt-4">
